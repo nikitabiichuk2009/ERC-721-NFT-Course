a68f2f97d94292865080ed535ca88e270914f487
Nikita Biichuk <niktestpython@gmail.com>
2024-12-03 23:41:33 +0200

    Update to the newest version

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.18;
+pragma solidity ^0.8.27;
 
 import {Script} from "forge-std/Script.sol";
 import {BasicNFT} from "../src/BasicNFT.sol";
--- src/MoodNft.sol
+++ src/MoodNft.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.18;
+pragma solidity ^0.8.27;
 
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import {Base64} from "@openzeppelin/contracts/utils/Base64.sol";
